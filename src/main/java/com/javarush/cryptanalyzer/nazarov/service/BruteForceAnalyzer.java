4c171acd05adaabfedd568d5de7ccada9db47632
R.N <rusnazus@gmail.com>
2023-09-14 00:12:38 +0300

    extract some methods in BruteForceAnalyzer

--- src/main/java/com/javarush/cryptanalyzer/nazarov/service/BruteForceAnalyzer.java
+++ src/main/java/com/javarush/cryptanalyzer/nazarov/service/BruteForceAnalyzer.java
@@ -2,7 +2,6 @@ package com.javarush.cryptanalyzer.nazarov.service;
 
 import com.javarush.cryptanalyzer.nazarov.entity.Result;
 import com.javarush.cryptanalyzer.nazarov.entity.ResultCode;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -11,17 +10,20 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import static com.javarush.cryptanalyzer.nazarov.constants.ActionsConstants.*;
 import static com.javarush.cryptanalyzer.nazarov.constants.CryptoAlphabet.ALPHABET;
 
 public class BruteForceAnalyzer implements Action {
+
+    private static int maxScore = 0;
+    private static int decodedTextPosition = 0;
 
     @Override
     public Result execute(String[] parameters) {
 
         String encodedFile = parameters[0], decodedFile = parameters[1];
         List<String> list;
+        StringBuilder stringBuilder = new StringBuilder();
 
         try {
             list = Files.readAllLines(Path.of(encodedFile));
@@ -29,12 +31,31 @@ public class BruteForceAnalyzer implements Action {
             return new Result(ResultCode.ERROR, BRUTEFORCE_ERROR);
         }
 
-        StringBuilder stringBuilder = new StringBuilder();
         for (String s : list) {
             stringBuilder.append(s).append('\n');
         }
         list.clear();
 
+        Map<Integer, String> textMap = getTextMap(stringBuilder);
+
+        Map<Integer, Integer> scoresMap = getScoresMap(textMap);
+
+        String realText = findRealText(textMap, scoresMap);
+
+        int realKey = ALPHABET.length - decodedTextPosition;
+        System.out.println(KEY_FOUND + realKey);
+
+        try (Writer writer = new FileWriter(decodedFile)
+        ) {
+            writer.write(realText.toCharArray());
+        } catch (IOException e) {
+            return new Result(ResultCode.ERROR, BRUTEFORCE_ERROR);
+        }
+        return new Result(ResultCode.OK, BRUTEFORCE_FINISHED);
+    }
+
+
+    private static Map<Integer, String> getTextMap(StringBuilder stringBuilder) {
         Map<Integer, String> textMap = new HashMap<>();
         textMap.put(0, stringBuilder.toString());
 
@@ -50,9 +71,12 @@ public class BruteForceAnalyzer implements Action {
             }
             textMap.put(i, stringBuilder.toString());
         }
+        return textMap;
+    }
 
+    private static Map<Integer, Integer> getScoresMap(Map<Integer, String> textMap) {
         Map<Integer, Integer> scoresMap = new HashMap<>();
-        int currentScore = 0, maxScore = 0;
+        int currentScore = 0;
 
         for (var entry : textMap.entrySet()) {
             Integer currentTextPosition = entry.getKey();
@@ -69,29 +93,17 @@ public class BruteForceAnalyzer implements Action {
             scoresMap.put(currentTextPosition, currentScore);
             currentScore = 0;
         }
+        return scoresMap;
+    }
 
-        String realText = "";
-        int decodedTextPosition = 0;
-
+    private static String findRealText(Map<Integer, String> textMap, Map<Integer, Integer> scoresMap) {
         for (var entry : scoresMap.entrySet()) {
             if (entry.getValue() == maxScore) {
                 decodedTextPosition = entry.getKey();
-                realText = textMap.get(decodedTextPosition);
-                break;
+                return textMap.get(decodedTextPosition);
             }
         }
-
-        try (Writer writer = new FileWriter(decodedFile)
-        ) {
-            writer.write(realText.toCharArray());
-        } catch (IOException e) {
-            return new Result(ResultCode.ERROR, BRUTEFORCE_ERROR);
-        }
-
-        int realKey = ALPHABET.length - decodedTextPosition;
-        System.out.println(KEY_FOUND + realKey);
-
-        return new Result(ResultCode.OK, BRUTEFORCE_FINISHED);
+        return textMap.get(decodedTextPosition);
     }
 }
 
